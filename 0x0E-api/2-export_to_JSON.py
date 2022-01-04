ebcbd438106b90c33d11d5f976b8081e769f696b
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-04 05:59:10 -0600

    task 2 complete

---
+++ 0x0E-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""gathers todo info based on input id"""
+from sys import argv
+import requests
+import json
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    user_info = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(user_id)).json()
+    username = user_info.get('username')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.
+        format(user_id)).json()
+
+    j_tasks = {user_id: []}
+
+    for task in tasks:
+        j_tasks[user_id].append({"task": task.get('title'),
+                                "completed": task.get('completed'),
+                                "username": username})
+
+    with open('{}.json'.format(user_id), 'w') as jsonfile:
+        json.dump(j_tasks, jsonfile)
