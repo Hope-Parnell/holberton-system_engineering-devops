0479bc032e40c86508ff45ef17a27a8641184fcf
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-04 06:14:33 -0600

    task 3 complete

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""gathers todo info based on input id"""
+import requests
+import json
+
+
+if __name__ == "__main__":
+    j_tasks = {}
+    users = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        user_info = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.
+            format(user_id)).json()
+        username = user_info.get('username')
+        tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'.
+            format(user_id)).json()
+
+        j_tasks[user_id] = []
+
+        for task in tasks:
+            j_tasks[user_id].append({"username": username,
+                                    "task": task.get('title'),
+                                    "completed": task.get('completed')})
+
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(j_tasks, jsonfile)
