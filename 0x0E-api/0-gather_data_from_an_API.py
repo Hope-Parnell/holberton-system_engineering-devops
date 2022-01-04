4f1b7ceca9b013850e0496b4d41bb624fa159ad4
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-04 05:23:45 -0600

    task 0 completed

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""gathers todo info based on input id"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    user_info = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(user_id)).json()
+    name = user_info.get('name')
+    tasks_completed = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos?completed=true'.
+        format(user_id)).json()
+    total_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.
+        format(user_id)).json()
+
+    print('Employee {} is done with tasks\
+          ({}/{})'.
+          format(name, len(tasks_completed), len(total_tasks)))
+    for item in tasks_completed:
+        print('\t {}'.format(item.get('title')))
